d7b7ffa659025056f87dc8b9601bd382c71fe2e0
PrinceSch <sofy95@mail.ru>
2021-06-15 23:36:37 +0700

    настроил текущие экраны как single activity

--- app/src/main/java/ru/geeekbrains/mynoteapp/MainActivity.java
+++ app/src/main/java/ru/geeekbrains/mynoteapp/MainActivity.java
@@ -10,13 +10,31 @@ import ru.geeekbrains.mynoteapp.ui.NoteDetailActivity;
 import ru.geeekbrains.mynoteapp.ui.NoteDetailFragment;
 import ru.geeekbrains.mynoteapp.ui.NoteListFragment;
 
+/*
+1. Подумайте о функционале вашего приложения заметок. Какие экраны там могут быть, помимо основного со списком заметок?
+Как можно использовать меню и всплывающее меню в вашем приложении? Не обязательно сразу пытаться реализовать весь этот
+функционал, достаточно создать макеты и структуру, а реализацию пока заменить на заглушки или всплывающие уведомления (Toast).
+Используйте подход Single Activity для отображения экранов.
+
+2. Создайте боковое навигационное меню для своего приложения и добавьте туда хотя бы один экран,
+например «Настройки» или «О приложении».
+
+3. * Создайте полноценный заголовок для NavigationDrawer’а. К примеру, аватарка пользователя,
+его имя и какая-то дополнительная информация.*/
 
 public class MainActivity extends AppCompatActivity implements NoteListFragment.OnNoteClicked {
+
+    public static final String ARG_NOTE = "ARG_NOTE";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        getSupportFragmentManager()
+                .beginTransaction()
+                .add(R.id.notes_fragment, new NoteListFragment(), "NoteListFragment")
+                .commit();
     }
 
     @Override
@@ -27,12 +45,14 @@ public class MainActivity extends AppCompatActivity implements NoteListFragment.
         if (isLandscape){
             getSupportFragmentManager()
                     .beginTransaction()
-                    .replace(R.id.notes_detail_fragment, NoteDetailFragment.newInstance(note))
+                    .replace(R.id.notes_fragment, NoteDetailFragment.newInstance(note))
                     .commit();
         } else {
-            Intent intent = new Intent(this, NoteDetailActivity.class);
-            intent.putExtra(NoteDetailActivity.ARG_NOTE, note);
-            startActivity(intent);
+            getSupportFragmentManager()
+                    .beginTransaction()
+                    .replace(R.id.notes_fragment, NoteDetailFragment.newInstance(note))
+                    .addToBackStack(null)
+                    .commit();
         }
     }
 }
