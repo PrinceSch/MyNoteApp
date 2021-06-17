e1315d9ba6b2ab22fa6569626d0dbc472e935fc8
PrinceSch <sofy95@mail.ru>
2021-06-17 16:29:33 +0700

    добавьте в боковое навигационное меню хотя бы один экран, например «Настройки» или «О приложении».

--- app/src/main/java/ru/geeekbrains/mynoteapp/MainActivity.java
+++ app/src/main/java/ru/geeekbrains/mynoteapp/MainActivity.java
@@ -5,6 +5,7 @@ import androidx.appcompat.app.ActionBar;
 import androidx.appcompat.app.ActionBarDrawerToggle;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.appcompat.widget.Toolbar;
+import androidx.core.view.GravityCompat;
 import androidx.drawerlayout.widget.DrawerLayout;
 
 import android.content.Intent;
@@ -15,6 +16,7 @@ import android.widget.Toast;
 import com.google.android.material.navigation.NavigationView;
 
 import ru.geeekbrains.mynoteapp.domain.Note;
+import ru.geeekbrains.mynoteapp.ui.AboutFragment;
 import ru.geeekbrains.mynoteapp.ui.NoteDetailActivity;
 import ru.geeekbrains.mynoteapp.ui.NoteDetailFragment;
 import ru.geeekbrains.mynoteapp.ui.NoteListFragment;
@@ -50,29 +52,38 @@ public class MainActivity extends AppCompatActivity implements NoteListFragment.
         drawerLayout.addDrawerListener(toggle);
         toggle.syncState();
 
+        getSupportFragmentManager()
+                .beginTransaction()
+                .add(R.id.notes_fragment, new NoteListFragment(), "NoteListFragment")
+                .commit();
+
         NavigationView navigationView = findViewById(R.id.menu_view);
         navigationView.setNavigationItemSelectedListener(new NavigationView.OnNavigationItemSelectedListener() {
             @Override
             public boolean onNavigationItemSelected(@NonNull MenuItem item) {
+                drawerLayout.closeDrawer(GravityCompat.START);
 
                 if(item.getItemId() == R.id.new_note){
                     Toast.makeText(getApplicationContext(), R.string.create_new_note, Toast.LENGTH_SHORT).show();
+                    return true;
                 } else if (item.getItemId() == R.id.favourites){
                     Toast.makeText(getApplicationContext(), R.string.favourite_notes, Toast.LENGTH_SHORT).show();
+                    return true;
                 } else if (item.getItemId() == R.id.settings){
                     Toast.makeText(getApplicationContext(), R.string.settings, Toast.LENGTH_SHORT).show();
+                    return true;
                 } else if (item.getItemId() == R.id.about){
-                    Toast.makeText(getApplicationContext(), R.string.about, Toast.LENGTH_SHORT).show();
+                    getSupportFragmentManager()
+                            .beginTransaction()
+                            .replace(R.id.notes_fragment, new AboutFragment(), "AboutFragment")
+                            .addToBackStack(null)
+                            .commit();
+                    return true;
                 }
 
                 return false;
             }
         });
-
-        getSupportFragmentManager()
-                .beginTransaction()
-                .add(R.id.notes_fragment, new NoteListFragment(), "NoteListFragment")
-                .commit();
     }
 
     @Override
