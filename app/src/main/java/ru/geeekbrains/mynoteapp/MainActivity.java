3d81a7cd59f5e95ec5a32dff0e3d9e4bfdb83d79
PrinceSch <sofy95@mail.ru>
2021-06-17 15:54:53 +0700

    Создайте боковое навигационное меню для своего приложения

--- app/src/main/java/ru/geeekbrains/mynoteapp/MainActivity.java
+++ app/src/main/java/ru/geeekbrains/mynoteapp/MainActivity.java
@@ -1,9 +1,18 @@
 package ru.geeekbrains.mynoteapp;
 
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.ActionBar;
+import androidx.appcompat.app.ActionBarDrawerToggle;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.Toolbar;
+import androidx.drawerlayout.widget.DrawerLayout;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.view.MenuItem;
+import android.widget.Toast;
+
+import com.google.android.material.navigation.NavigationView;
 
 import ru.geeekbrains.mynoteapp.domain.Note;
 import ru.geeekbrains.mynoteapp.ui.NoteDetailActivity;
@@ -30,6 +39,35 @@ public class MainActivity extends AppCompatActivity implements NoteListFragment.
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        DrawerLayout drawerLayout = findViewById(R.id.main_screen);
+        Toolbar toolbar = findViewById(R.id.toolbar);
+
+        ActionBarDrawerToggle toggle = new ActionBarDrawerToggle(
+                this, drawerLayout, toolbar, R.string.app_name, R.string.app_name
+        );
+
+        drawerLayout.addDrawerListener(toggle);
+        toggle.syncState();
+
+        NavigationView navigationView = findViewById(R.id.menu_view);
+        navigationView.setNavigationItemSelectedListener(new NavigationView.OnNavigationItemSelectedListener() {
+            @Override
+            public boolean onNavigationItemSelected(@NonNull MenuItem item) {
+
+                if(item.getItemId() == R.id.new_note){
+                    Toast.makeText(getApplicationContext(), R.string.create_new_note, Toast.LENGTH_SHORT).show();
+                } else if (item.getItemId() == R.id.favourites){
+                    Toast.makeText(getApplicationContext(), R.string.favourite_notes, Toast.LENGTH_SHORT).show();
+                } else if (item.getItemId() == R.id.settings){
+                    Toast.makeText(getApplicationContext(), R.string.settings, Toast.LENGTH_SHORT).show();
+                } else if (item.getItemId() == R.id.about){
+                    Toast.makeText(getApplicationContext(), R.string.about, Toast.LENGTH_SHORT).show();
+                }
+
+                return false;
+            }
+        });
 
         getSupportFragmentManager()
                 .beginTransaction()
